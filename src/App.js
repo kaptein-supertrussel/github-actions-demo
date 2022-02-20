5209c2c584f9010b02414336031b9c7f426696df
kaptein-supertrussel <kaptein.supertrussel@gmail.com>
2022-02-20 13:38:16 +0100

    Created React demo-app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import kaptein from './kaptein-supertrussel.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={kaptein} className="App-logo" alt="kaptein" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
